e6c41b591d3443d386a6874ebf84884f68343ed9
Andrey Miskov <amiskov@gmail.com>
2021-10-18 20:27:14 +0300

    Fix prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,31 +4,25 @@ from brain_games.games.game_types import Game
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def is_prime(num) -> bool:
+def is_prime(n) -> bool:
     """
     Returns `True` if `num` is a prime number:
 
     >>> is_prime(1)
     False
-    >>> is_prime(8)
-    False
     >>> is_prime(2)
     True
-    >>> is_prime(20)
-    False
     >>> is_prime(17)
     True
+    >>> is_prime(22)
+    False
     """
-    if num < 2:
+    if n <= 1:
         return False
-    elif num == 2:
-        return True
-    else:
-        for i in range(2, num // 2):
-            if num % i == 0:
-                return False
-            else:
-                return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 
 def create_game() -> Game:
