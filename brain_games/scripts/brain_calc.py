9399db654731ad05a7ce07d3bb419c2072976cbf
Andrey Miskov <amiskov@gmail.com>
2021-10-18 20:16:17 +0300

    Fix calc game

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from brain_games.cli import welcome_user
 from brain_games.gameplay import run
-from brain_games.games.calc import rules, create_game, evaluate_str_expression
+from brain_games.games.calc import rules, create_game
 
 
 def main():
     name = welcome_user()
-    run(rules, create_game, evaluate_str_expression, name)
+    run(rules, create_game, name)
 
 
 if __name__ == '__main__':
